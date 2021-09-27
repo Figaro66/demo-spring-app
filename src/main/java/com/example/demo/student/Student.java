27e3091659a0cd061243481867c9850fdd77db7a
Figaro <farago.david92@gmail.com>
2021-09-27 22:42:10 +0200

    added lombok to project

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,11 +1,16 @@
 package com.example.demo.student;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
 public class Student {
     @Id
     @SequenceGenerator(
@@ -17,15 +22,12 @@ public class Student {
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
-    private Long id;
-    private String name;
-    private String email;
-    private LocalDate dob;
+    Long id;
+    String name;
+    String email;
+    LocalDate dob;
     @Transient
-    private Integer age;
-
-    public Student() {
-    }
+    Integer age;
 
     public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
@@ -40,54 +42,8 @@ public class Student {
         this.dob = dob;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
     public Integer getAge() {
         return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
-    }
 }
